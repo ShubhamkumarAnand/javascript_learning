48f33b9f252515826eb1bff1751d3cb1c36e3db8
imskanand <the.anand3099@gmail.com>
2022-11-05 11:33:46 +0530

    Until Logical Operation

---
+++ app.js
@@ -0,0 +1,38 @@
+// To use the code in Modern Way
+'use strict'
+
+// This created a alert box in the Browser
+console.log("Javascript is Awesome!")
+
+// Variables
+let name = 'Shubham'
+const age = 22
+
+// Capital letters for constants for the Hard Coded values
+const PI = 2.71
+
+// Data Types in Javascript
+
+console.log(Infinity)
+console.log(NaN ** 0);
+
+// null -> value unknown | undefined -> value undefined or not assigned
+
+let enteredName = prompt("what is your name?")
+alert(`Your name is ${enteredName}`)
+
+console.log("" + 5 + "8");
+console.log(+" ");
+console.log(+"0");
+
+/*
+  ! Operator Precedence
+  increment && decrement operator -> ++/--
+  unary operator -> +,-
+  exponential operator -> **
+  multiplication -> *
+  Division -> /
+  addition -> +
+  subtraction -> -
+  assignment -> =
+*/
